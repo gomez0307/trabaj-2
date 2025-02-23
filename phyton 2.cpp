9808ecaa7427218beb20b83fcf6192783e7901d6
gomez0307 <thomasgomez0307@gmail.com>
2025-02-23 00:20:40 -0500

    Add files via upload

---
+++ phyton 2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+// 1. Recibir el número del usuario
+double numero;
+cin >> numero;
+
+// 2. Multiplicarlo por 2
+double doble = numero * 2;
+
+
+// 3. Mostrar el resultado
+cout << doble << endl;
+
+return 0;
+
+
+
+
+ numero;
+cin >> numero;
+
+// 2. Multiplicarlo por 2
+double triple = numero * 3;
+
+
+
+// 3. Mostrar el resultado
+cout << triple << endl;
+
+return 0;
+}
